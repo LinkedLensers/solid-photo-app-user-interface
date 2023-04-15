5409242a1e7afb751117536a40dee49bd412e6f3
JSteinbach <jsteinbach@mailbox.org>
2023-04-15 16:43:36 +0200

    Add (dummy) upload files button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import './App.css';
-import Header from './components/Header';
-import Gallery from './components/Gallery';
-import Comunica from './components/Comunica';
+import "./App.css";
+import Header from "./components/Header";
+import Gallery from "./components/Gallery";
+import Comunica from "./components/Comunica";
+
+// TODO: login
 
 function App() {
-	return (
-		<div className='bg-blue-100 w-screen h-screen'>
-			<div className='container h-screen mx-auto px-4 bg-red-100'>
-				<Header />
-				<Gallery />
-				<Comunica />
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-blue-100 w-screen h-screen">
+      <div className="container h-screen mx-auto px-4 bg-red-100">
+        <Header />
+        <Gallery />
+        <Comunica />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,14 @@
 import beadyEyes from '../assets/beady-eyes.png'
+import Button from "./_Button";
 
 const Gallery = () => {
+
+	// TODO: filepicker/upload
+	 //
+	 //
+    const uploadFiles = () => {
+        console.log('uploadFiles clicked')
+    }
 
 	const n = 6; // Or something else
 
@@ -13,13 +21,14 @@ const Gallery = () => {
 			<p className="">
 				Gallery
 			</p>
-        <div className='columns-md gap-0'>
-			{
-				// create element n times
-				// https://stackoverflow.com/questions/34189370/how-to-repeat-an-element-n-times-using-jsx-and-lodash
-				[...Array(n)].map((e, i) => img)
-			}
-</div>
+        <Button click={uploadFiles} style='bg-black' name='Upload' />
+			<div className='columns-md gap-0'>
+				{
+					// create element n times
+					// https://stackoverflow.com/questions/34189370/how-to-repeat-an-element-n-times-using-jsx-and-lodash
+					[...Array(n)].map((e, i) => img)
+				}
+			</div>
 		</div>
 	);
 
