59b66af95e735286f483e2e2d940295b8969ecda
JSteinbach <jsteinbach@mailbox.org>
2023-04-24 20:58:51 +0200

    Add Solid Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,79 @@ import "./App.css";
 import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 import Comunica from "./components/Comunica";
-import {initialize} from "./solid-backend/index";
-// TODO: login
+import { initialize } from "./solid-backend/index";
+
+import {
+  handleIncomingRedirect,
+  getDefaultSession,
+} from "@inrupt/solid-client-authn-browser";
+import { useEffect, useState } from "react";
+import SolidAuth from "./components/SolidAuth";
 
 function App() {
+  const session = getDefaultSession();
+
+  // De loggedIn variabele houdt de login status bij,
+  // en update de pagina wanneer de status verandert.
+  const [loggedIn, setLoggedIn] = useState(session.info.isLoggedIn);
+
+  // De checkingLogin variabele houdt bij of onze initiële
+  // check voor login informatie is afgerond.
+  const [checkingLogin, setCheckingLogin] = useState(true);
+
+  // Deze functie voert uit bij het updaten van de component
+  useEffect(() => {
+    // Forceer hernieuwen van de pagina bij het veranderen van de login status
+    session.onLogin(() => setLoggedIn(true));
+    session.onLogout(() => setLoggedIn(false));
+
+    // Deze functie gaat na of we teruggestuurd zijn
+    // naar de huidige pagina door de Solid login pagina.
+    handleIncomingRedirect({ restorePreviousSession: true })
+      .then((info) => {
+        // Update de status van de component voor
+        // de login status en de login check status
+        // op basis van het resultaat van de functie.
+        // Voor meer informatie kan je de documentatie bekijken op
+        // https://docs.inrupt.com/developer-tools/api/javascript/solid-client-authn-browser/functions.html#handleincomingredirect
+        let status = info?.isLoggedIn || false;
+        if (status !== loggedIn) setLoggedIn(status);
+        if (info) setCheckingLogin(false);
+      })
+      .catch(console.error);
+  });
+
   return (
     <div className="bg-blue-100 w-screen h-screen">
       <div className="container h-screen mx-auto px-4 bg-red-100">
         <Header />
-        <Gallery />
-          <button onClick={async event => {
-              await initialize({webid: "https://woslabbi.pod.knows.idlab.ugent.be/profile/card#me", fetch: fetch})
-          }}>testInit</button>
-        {/* <Comunica /> */}
+        <div>
+          {checkingLogin ? (
+            <p>Loading Session information ...</p>
+          ) : (
+            <div>
+              <SolidAuth />
+              {loggedIn && (
+                <div>
+                  <Gallery />
+
+                  <button
+                    onClick={async (event) => {
+                      await initialize({
+                        webid:
+                          "https://woslabbi.pod.knows.idlab.ugent.be/profile/card#me",
+                        fetch: fetch,
+                      });
+                    }}
+                  >
+                    testInit
+                  </button>
+                  <Comunica />
+                </div>
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/SolidAuth.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { getDefaultSession } from "@inrupt/solid-client-authn-browser"
+import { useState, useEffect } from 'react';
+
+export default function SolidAuth() {
+  let session = getDefaultSession()
+
+  const [isLoggedIn, setIsLoggedIn] = useState(session.info.isLoggedIn)
+
+  useEffect(() => {
+    session.onLogin(() => setIsLoggedIn(true))
+    session.onLogout(() => setIsLoggedIn(false))
+  }, [session])
+
+  // Handle Login call
+  function handleLogin(e) {
+    e.preventDefault();
+
+    // get Identity Provider value from text field
+    let idp = e.target[0].value
+
+    // Make login call
+    session.login({
+      oidcIssuer: idp,
+      redirectUrl: window.location.href,
+      clientName: "Webdevelopment Practicum 3 React Template"
+    })
+  }
+
+  function handleLogout(e) {
+    e.preventDefault();
+    session.logout()
+  }
+
+  if (!isLoggedIn) {
+    // If session is not logged in, show the login screen
+    return (
+      <div >
+        <form onSubmit={handleLogin} style={{
+          "display": "flex",
+          "margin": "10%",
+          "height": "100%",
+          "flexDirection": "column"
+        }}>
+          <input type="text" placeholder="identity provider" style={{
+              marginBottom: "1em"
+          }}/>
+          <input type="submit" value="Login"/>
+        </form>
+      </div>
+    )
+  } else {
+    // If session is logged in, you should not be here
+    return (
+      <div>
+        <p style={{display: "inline-block", marginRight: "1em"}}>
+          Logged in as: { session.info.webId }
+        </p>
+        <button onClick={ handleLogout }>Logout</button>
+      </div>
+
+    )
+  }
+}
