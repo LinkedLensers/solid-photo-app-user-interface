f48a3154dada9f784f2fed034b88ddc65651d08b
JSteinbach <jsteinbach@mailbox.org>
2023-04-26 21:23:54 +0200

    Remove dutch comments :)

--- src/App.jsx
+++ src/App.jsx
@@ -14,31 +14,18 @@ import SolidAuth from "./components/SolidAuth";
 function App() {
   const session = getDefaultSession();
 
-  // De loggedIn variabele houdt de login status bij,
-  // en update de pagina wanneer de status verandert.
   const [loggedIn, setLoggedIn] = useState(session.info.isLoggedIn);
-
-  // De checkingLogin variabele houdt bij of onze initiële
-  // check voor login informatie is afgerond.
   const [checkingLogin, setCheckingLogin] = useState(true);
 
-  // Deze functie voert uit bij het updaten van de component
   useEffect(() => {
-    // Forceer hernieuwen van de pagina bij het veranderen van de login status
     session.onLogin(() => setLoggedIn(true));
     session.onLogout(() => setLoggedIn(false));
 
     console.log(session);
 
-    // Deze functie gaat na of we teruggestuurd zijn
-    // naar de huidige pagina door de Solid login pagina.
     handleIncomingRedirect({ restorePreviousSession: true })
       .then((info) => {
-        // Update de status van de component voor
-        // de login status en de login check status
-        // op basis van het resultaat van de functie.
-        // Voor meer informatie kan je de documentatie bekijken op
-        // https://docs.inrupt.com/developer-tools/api/javascript/solid-client-authn-browser/functions.html#handleincomingredirect
+       // https://docs.inrupt.com/developer-tools/api/javascript/solid-client-authn-browser/functions.html#handleincomingredirect
         let status = info?.isLoggedIn || false;
         if (status !== loggedIn) setLoggedIn(status);
         if (info) setCheckingLogin(false);
