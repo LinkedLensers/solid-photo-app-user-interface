a0fe603c05e490a028020210ab85bd35d9efd6d5
woutslabbinck <wout.slabbinck@ugent.be>
2023-04-26 21:29:03 +0200

    fix: add craco file

---
+++ craco.config.js
@@ -0,0 +1,25 @@
+const webpack = require("webpack");
+// Cannot find module 'stream' react webpack
+// https://github.com/diegomura/react-pdf/issues/1645#issuecomment-999072302
+module.exports = {
+    webpack: {
+        configure: {
+            resolve: {
+                fallback: {
+                    process: require.resolve("process/browser"),
+                    zlib: require.resolve("browserify-zlib"),
+                    stream: require.resolve("stream-browserify"),
+                    util: require.resolve("util"),
+                    buffer: require.resolve("buffer"),
+                    asset: require.resolve("assert"),
+                },
+            },
+            plugins: [
+                new webpack.ProvidePlugin({
+                    Buffer: ["buffer", "Buffer"],
+                    process: "process/browser",
+                }),
+            ],
+        },
+    },
+};
